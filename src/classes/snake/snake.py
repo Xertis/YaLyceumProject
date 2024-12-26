a67806bb32eb7de695bce177509c0234ec42086f
Xertis <xertis3785@gmail.com>
2024-12-26 14:14:06 +0300

    добавил обработку столкновений головы и хвоста

--- src/classes/snake/snake.py
+++ src/classes/snake/snake.py
@@ -9,7 +9,7 @@ class SNAKE:
         self.old_pos = [0, 0]
         self.radius = radius
         self.controller = CONTROLLER(radius, 5)
-        self.tail = [[0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0]] # Для теста хвоста, в идеале, сегменты должны добавляться после съеденного яблока
+        self.tail = [[0, 105], [0, 105], [0, 105], [0, 105], [0, 105], [0, 105], [0, 105], [0, 105], [0, 105]] # Для теста хвоста, в идеале, сегменты должны добавляться после съеденного яблока
         self.map = map
 
     def tail_move(self):
@@ -23,7 +23,7 @@ class SNAKE:
             cell_x = cell[0]
             cell_y = cell[1]
 
-            if distance.euclidean(cell, [old_x, old_y]) < self.radius // 2:
+            if distance.euclidean(cell, [old_x, old_y]) < self.radius * 1.7:
                 break
 
             self.tail[i] = [old_x, old_y]
@@ -37,6 +37,12 @@ class SNAKE:
 
         pos[0] = max(self.radius, min(pos[0], map_size[0] - self.radius))
         pos[1] = max(self.radius, min(pos[1], map_size[1] - self.radius))
+
+        # Самая простая реализацию обработки столкновений головы змейки с телом
+        for i in range(1, len(self.tail)):
+            segment = self.tail[i]
+            if distance.euclidean(segment, pos) < self.radius*2-1.5:
+                return
 
         if pos != self.pos:
             self.old_pos = self.pos[:]
