9d7664fa3e35047ca13c6a67cf226c92e53f0944
Xertis <xertis3785@gmail.com>
2024-12-31 01:46:50 +0300

    адаптировал LOADER под pyinstaller

--- src/classes/sprites_loader.py
+++ src/classes/sprites_loader.py
@@ -1,10 +1,17 @@
 from pygame import image, surface, transform
+import sys
 import os
 
 class LOADER:
     @staticmethod
     def load(path, size: tuple=None):
-        path = os.path.join(os.getcwd(), "res", "sprites", path)
+
+        if hasattr(sys, '_MEIPASS'):
+            base_path = sys._MEIPASS
+        else:
+            base_path = os.path.abspath(".")
+
+        path = os.path.join(base_path, "res", "sprites", path)
 
         if os.path.exists(path):
             sprite = image.load(path)
