6a8a9a35faf56ff757cfe6b660ad1711dd8d33d4
Fantasy2mind <Makskozlov2fantasy@gmail.com>
2025-01-12 16:28:12 +0300

    Правка и добавление файла шрифта

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame
 import sys
 from src.constants import SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_NAME, FPS
 from src.classes.map.map import MAP
+from src.classes.windows.start_window import DrawStartWindow
 
 pygame.init()
 pygame.display.set_caption(SCREEN_NAME)
