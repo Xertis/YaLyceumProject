d38c342e0ade0bdbe1d889d56fd46c8f6d7d678f
Fantasy2mind <Makskozlov2fantasy@gmail.com>
2025-01-12 14:41:56 +0300

    Добавил прототип стартового окна

---
+++ src/classes/windows/start_window.py
@@ -0,0 +1,33 @@
+import pygame
+
+class DrawStartWindow:
+    def __init__(self, scr, map):
+        self.is_play = False
+        self.screen = scr
+        self.map = map
+        self.font_jersey100 = pygame.font.Font('res/fonts/Jersey10-Regular.ttf', 100)
+        self.font_jersey40 = pygame.font.Font('res/fonts/Jersey10-Regular.ttf', 60)
+        self.draw()
+
+    def draw(self):
+        pygame.Surface.fill(self.screen, 'black')
+        name_of_game = self.font_jersey100.render('Gravity Snake', False, (178, 102, 255))
+        start_button = self.font_jersey40.render('Start game', True, (229, 204, 255))
+        rating_button = self.font_jersey40.render('Rating', True, (229, 204, 255))
+        self.screen.blit(name_of_game, (160, 50))
+        self.screen.blit(start_button, (280, 200))
+        self.screen.blit(rating_button, (333, 300))
+
+        start_button_rect = pygame.Rect(275, 195, 250, 70)
+
+        pos = pygame.mouse.get_pos()
+        if start_button_rect.collidepoint(pos):
+            rect = pygame.Rect(275, 195, 250, 70)
+            pygame.draw.rect(self.screen, 'black', rect)
+            font = pygame.font.Font('res/fonts/Jersey10-Regular.ttf', 65)
+            start_button2 = font.render('Start game', True, (229, 204, 255))
+            self.screen.blit(start_button2, (270, 197))
+
+        if pygame.mouse.get_pressed()[0] == True and start_button_rect.collidepoint(pos):
+            self.is_play = True
+            self.map.draw()
