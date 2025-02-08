12750b95e003318095450ee4c50749ce265052c9
urpro51 <zakharevskii.yurii@gmail.com>
2025-02-08 19:07:51 +0300

    Update main.py
    добавил рейтинг

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ from src.classes.windows.options import OPTIONS
 from src.classes.windows.rating import RATING
 from src.classes.windows.pause_window import PAUSE
 from src.classes.Tutorial import TUTORIAL
+import json
+import os
 pygame.mixer.pre_init(frequency=44100, size=-16, channels=2, buffer=512, devicename=None)
 pygame.init()
 pygame.display.set_caption(SCREEN_NAME)
@@ -18,6 +20,24 @@ map_window = MAP(SCREEN_WIDTH, SCREEN_HEIGHT, screen, start_window)
 rating_window = RATING(screen, start_window)
 pause_window = PAUSE(screen, start_window)
 clock = pygame.time.Clock()
+
+
+SCORES_FILE = "scores.json"
+
+
+def save_score(score):
+    if not os.path.exists(SCORES_FILE):
+        with open(SCORES_FILE, 'w') as file:
+            json.dump([], file)
+
+    with open(SCORES_FILE, 'r+') as file:
+        scores = json.load(file)
+        scores.append(score)
+        scores = sorted(scores, reverse=True)[:10]  # Топ-10 результатов
+        file.seek(0)
+        json.dump(scores, file)
+        file.truncate()
+
 
 
 map_window.generate()
@@ -48,6 +68,7 @@ while running:
     pos = pygame.mouse.get_pos()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
+            save_score(map_window.snake.score)  # Сохраняем результат при выходе
             running = False
         if event.type == pygame.MOUSEBUTTONDOWN and options_window.increase_volume_button_rect.collidepoint(pos):
             if not options_window.volume >= 0.99:
