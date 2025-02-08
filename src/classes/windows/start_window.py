cf978274b6d14b14b5dc640ffd3c865bbec45c76
urpro51 <zakharevskii.yurii@gmail.com>
2025-02-08 18:26:06 +0300

    Update start_window.py

--- src/classes/windows/start_window.py
+++ src/classes/windows/start_window.py
@@ -12,7 +12,7 @@ class DrawStartWindow:
         self.is_rating = False
         self.is_pause = False
         self.screen = scr
-
+        self.is_tutorial = False
         self.font_jersey100 = LOADER.font.load(FONT_PATH, 100)
         self.font_jersey60 = LOADER.font.load(FONT_PATH, 60)
         self.font_jersey65 = LOADER.font.load(FONT_PATH, 65)
@@ -26,61 +26,42 @@ class DrawStartWindow:
     def draw(self):
         pygame.Surface.fill(self.screen, 'black')
 
+        # Заголовок игры
         name_of_game = self.font_jersey100.render(SCREEN_NAME, False, (178, 102, 255))
+        name_rect = name_of_game.get_rect(center=(SCREEN_WIDTH // 2, SCREEN_HEIGHT * 0.1))
+        self.screen.blit(name_of_game, name_rect)
+
+        # Кнопки меню
         start_button = self.font_jersey60.render(TEXTS['start'], True, (229, 204, 255))
+        start_rect = start_button.get_rect(center=(SCREEN_WIDTH // 2, SCREEN_HEIGHT * 0.4))
+        self.screen.blit(start_button, start_rect)
+
+        tutorial_button = self.font_jersey60.render('tutorial', True, (229, 204, 255))
+        tutorial_rect = tutorial_button.get_rect(center=(SCREEN_WIDTH // 2, SCREEN_HEIGHT * 0.5))
+        self.screen.blit(tutorial_button, tutorial_rect)
+
         rating_button = self.font_jersey60.render(TEXTS['rating'], True, (229, 204, 255))
+        rating_rect = rating_button.get_rect(center=(SCREEN_WIDTH // 2, SCREEN_HEIGHT * 0.6))
+        self.screen.blit(rating_button, rating_rect)
+
         options_button = self.font_jersey60.render(TEXTS['options'], True, (229, 204, 255))
+        options_rect = options_button.get_rect(center=(SCREEN_WIDTH // 2, SCREEN_HEIGHT * 0.7))
+        self.screen.blit(options_button, options_rect)
 
-        name_width = name_of_game.get_width()
-        name_x = (SCREEN_WIDTH - name_width) / 2
-        name_y = SCREEN_HEIGHT * 0.1
+        # Обработка кликов
+        mouse_pos = pygame.mouse.get_pos()
+        if start_rect.collidepoint(mouse_pos):
+            if pygame.mouse.get_pressed()[0]:
+                self.is_play = True
 
-        start_button_width = start_button.get_width()
-        start_button_x = (SCREEN_WIDTH - start_button_width) / 2
-        start_button_y = SCREEN_HEIGHT * 0.4
+        if tutorial_rect.collidepoint(mouse_pos):
+            if pygame.mouse.get_pressed()[0]:
+                self.is_tutorial = True
 
-        rating_button_width = rating_button.get_width()
-        rating_button_x = (SCREEN_WIDTH - rating_button_width) / 2
-        rating_button_y = SCREEN_HEIGHT * 0.5
+        if rating_rect.collidepoint(mouse_pos):
+            if pygame.mouse.get_pressed()[0]:
+                self.is_rating = True
 
-        options_button_width = rating_button.get_width()
-        options_button_x = (SCREEN_WIDTH - options_button_width) / 2.06
-        options_button_y = SCREEN_HEIGHT * 0.6
-
-        self.screen.blit(start_button, (start_button_x, start_button_y))
-        self.screen.blit(rating_button, (rating_button_x, rating_button_y))
-        self.screen.blit(name_of_game, (name_x, name_y))
-        self.screen.blit(options_button, (options_button_x, options_button_y))
-
-        start_button_rect = pygame.Rect(start_button_x - 10, start_button_y - 5, start_button_width + 20, 70)
-        rating_button_rect = pygame.Rect(rating_button_x - 10, rating_button_y + 10, rating_button_width + 17, 45)
-        options_button_rect = pygame.Rect(options_button_x - 10, options_button_y - 5, options_button_width + 40, 70)
-
-        pos = pygame.mouse.get_pos()
-        if start_button_rect.collidepoint(pos):
-            pygame.draw.rect(self.screen, 'black', start_button_rect)
-
-            start_button2 = self.font_jersey65.render(TEXTS['start'], True, (229, 204, 255))
-            self.screen.blit(start_button2, (start_button_x - 7, start_button_y - 3))
-
-        if rating_button_rect.collidepoint(pos):
-            pygame.draw.rect(self.screen, 'black', rating_button_rect)
-
-            rating_button2 = self.font_jersey65.render(TEXTS['rating'], True, (229, 204, 255))
-            self.screen.blit(rating_button2, (rating_button_x - 5, rating_button_y - 3))
-
-        if options_button_rect.collidepoint(pos):
-            pygame.draw.rect(self.screen, 'black', options_button_rect)
-
-            options_button2 = self.font_jersey65.render(TEXTS['options'], True, (229, 204, 255))
-            self.screen.blit(options_button2, (options_button_x - 5, options_button_y - 3))
-
-        if pygame.mouse.get_pressed()[0] == True and start_button_rect.collidepoint(pos):
-            pygame.mixer.Channel(0).pause()
-            self.is_play = True
-
-        if pygame.mouse.get_pressed()[0] == True and rating_button_rect.collidepoint(pos):
-            self.is_rating = True
-
-        if pygame.mouse.get_pressed()[0] == True and options_button_rect.collidepoint(pos):
-            self.is_options = True
+        if options_rect.collidepoint(mouse_pos):
+            if pygame.mouse.get_pressed()[0]:
+                self.is_options = True
